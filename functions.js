d8f473c19a2fc5cd1bae0c7b5b7fdbfc4a543fe5
dauda1995 <dauda933@gmail.com>
2018-12-01 21:14:35 +0100

    added comments and moderator

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 class User{
-    constructor(name){
+    constructor(name, comment){
         this._name = name;
-
+        this._comment = comment;
     }
     get name(){
         return this._name;
@@ -9,21 +9,34 @@ class User{
     set name(value){
         this._name = value;
     }
-    isLoggedIn(){
-        let date = new Date();
-        return `${date.getDay}:${date.getHours}/${date.getMinutes}/${date.getSeconds}`
+    get comment(){
+        return `${this._name} posts :${this._comment}`;
+    }
+    set comment(str){
+        this._comment = str;
     }
     isLoggedIn(){
         let date = new Date();
         return `${date.getDay}:${date.getHours}/${date.getMinutes}/${date.getSeconds}`
     }
-   
-
-
-    
+    isLoggedIn(){
+        let date = new Date();
+        return `${date.getDay}:${date.getHours}/${date.getMinutes}/${date.getSeconds}`
+    }
+       
 }
+
 class Moderator extends User{
-    constructor()
+    constructor(priviledge){
+        super(name,comment);
+        this._priviledge = priviledge;
+
+    }
+
+    delete_comment(arr, commentId){
+        arr.splice(commentid -1, 1);
+        
+    }
     //include an object for moderator, should have the ability to delete comments 
 }
 
